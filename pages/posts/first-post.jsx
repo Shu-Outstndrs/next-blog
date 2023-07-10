a330e57c37e607e5ad32e6b469d47e01e29016a4
Shu-Outstndrs <shu.watanabe111@gmail.com>
2023-07-10 20:13:48 +0900

    test

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -35,7 +35,7 @@ export default function FirstPost() {
         <Link href="/">← Back to home</Link>
       </h2>
       <LikeButton />
-      <Outstndrs members={["so", "shu", "miya", "yuta", "iuto", "shopi"]} />
+      <Outstndrs members={["so", "shu", "miya", "yuta", "iuto", "shopi", ""]} />
     </Layout>
   );
 }
